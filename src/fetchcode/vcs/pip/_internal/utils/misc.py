0faa5a1f9901f9d307e240e2dde3e90a9beff34a
Alexander Mazuruk <a.mazuruk@samsung.com>
2021-09-29 03:48:46 +0200

    remove unused func from misc
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- src/fetchcode/vcs/pip/_internal/utils/misc.py
+++ src/fetchcode/vcs/pip/_internal/utils/misc.py
@@ -486,33 +486,6 @@ def hide_url(url):
     return HiddenText(url, redacted=redacted)
 
 
-def protect_pip_from_modification_on_windows(modifying_pip):
-    # type: (bool) -> None
-    """Protection of pip.exe from modification on Windows
-
-    On Windows, any operation modifying pip should be run as:
-        python -m pip ...
-    """
-    pip_names = [
-        "pip.exe",
-        "pip{}.exe".format(sys.version_info[0]),
-        "pip{}.{}.exe".format(*sys.version_info[:2]),
-    ]
-
-    # See https://github.com/pypa/pip/issues/1299 for more discussion
-    should_show_use_python_msg = (
-        modifying_pip and WINDOWS and os.path.basename(sys.argv[0]) in pip_names
-    )
-
-    if should_show_use_python_msg:
-        new_command = [sys.executable, "-m", "pip"] + sys.argv[1:]
-        raise CommandError(
-            "To modify pip, please run the following command:\n{}".format(
-                " ".join(new_command)
-            )
-        )
-
-
 def is_console_interactive():
     # type: () -> bool
     """Is this console interactive?"""
